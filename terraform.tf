ec3c1d48526b525c59d6104c06812f40e4f91871
Joakim Ohman <joakim.ohman@elastx.se>
2020-03-11 21:57:24 +0000

    updated to support Terraform v0.12

--- terraform.tf
+++ terraform.tf
@@ -6,13 +6,13 @@ variable "user_name" {}
 variable "tenant_name" {}
 variable "az_list" {
   description = "List of Availability Zones available in your OpenStack cluster"
-  type = "list"
+  type = list
   default = ["sto1", "sto2", "sto3"]
 }
 
 # A little bit hackish, but for demo purposes; why not
 variable "cloudconfig_web" {
-  type = "string"
+  type = string
   default = <<EOF
 #cloud-config
 system_info:
@@ -23,7 +23,7 @@ packages:
 EOF
 }
 variable "cloudconfig_db" {
-  type = "string"
+  type = string
   default = <<EOF
 #cloud-config
 system_info:
@@ -38,10 +38,10 @@ EOF
 ### [Elastx Openstack] ###
 
 provider "openstack" {
-  user_name = "${var.user_name}"
-  tenant_name = "${var.tenant_name}"
-  password = "${var.password}"
-  auth_url = "https://ops.elastx.cloud:5000/v2.0"
+  user_name = var.user_name
+  tenant_name = var.tenant_name
+  password = var.password
+  auth_url = "https://ops.elastx.cloud:5000/v3"
 }
 
 ### [General setup] ###
@@ -49,12 +49,12 @@ provider "openstack" {
 resource "openstack_networking_router_v2" "router" {
   name = "demo-router"
   admin_state_up = "true"
-  external_gateway = "600b8501-78cb-4155-9c9f-23dfcba88828"
+  external_network_id = "600b8501-78cb-4155-9c9f-23dfcba88828"
 }
 
 resource "openstack_compute_keypair_v2" "demo_keypair" {
   name = "demo-keypair"
-  public_key = "${file("demo_rsa.pub")}"
+  public_key = file("demo_rsa.pub")
 }
 
 ### Should be tighten up, not let the world be able to ssh
@@ -95,7 +95,7 @@ resource "openstack_compute_secgroup_v2" "db_sg" {
     from_port = 3306
     to_port = 3306
     ip_protocol = "tcp"
-    cidr = "${openstack_networking_subnet_v2.web_subnet.cidr}"
+    cidr = openstack_networking_subnet_v2.web_subnet.cidr
   }
 }
 
@@ -108,7 +108,7 @@ resource "openstack_networking_network_v2" "web_net" {
 
 resource "openstack_networking_subnet_v2" "web_subnet" {
   name = "demo-web-subnet"
-  network_id = "${openstack_networking_network_v2.web_net.id}"
+  network_id = openstack_networking_network_v2.web_net.id
   cidr = "10.0.0.0/24"
   ip_version = 4
   enable_dhcp = "true"
@@ -116,8 +116,8 @@ resource "openstack_networking_subnet_v2" "web_subnet" {
 }
 
 resource "openstack_networking_router_interface_v2" "web-ext-interface" {
-  router_id = "${openstack_networking_router_v2.router.id}"
-  subnet_id = "${openstack_networking_subnet_v2.web_subnet.id}"
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.web_subnet.id
 }
 
 resource "openstack_compute_floatingip_v2" "fip" {
@@ -135,24 +135,25 @@ resource "openstack_compute_servergroup_v2" "web_srvgrp" {
 resource "openstack_compute_instance_v2" "web_cluster" {
   name = "demo-web-${count.index+1}"
   count = "2"
-  availability_zone = "${element(var.az_list, count.index)}"
+  availability_zone = element(var.az_list, count.index)
   image_name = "ubuntu-16.04-server-latest"
   flavor_name = "v1-mini-1"
-  network = { 
-    uuid = "${openstack_networking_network_v2.web_net.id}"
+  network  { 
+    uuid = openstack_networking_network_v2.web_net.id
   }
-  key_pair = "${openstack_compute_keypair_v2.demo_keypair.name}"
+  key_pair = openstack_compute_keypair_v2.demo_keypair.name
   scheduler_hints {
-    group = "${openstack_compute_servergroup_v2.web_srvgrp.id}"
+    group = openstack_compute_servergroup_v2.web_srvgrp.id
   }
   security_groups = ["${openstack_compute_secgroup_v2.ssh_sg.name}","${openstack_compute_secgroup_v2.web_sg.name}"]
-  user_data = "${var.cloudconfig_web}"
+  user_data = var.cloudconfig_web
+  depends_on = [openstack_networking_subnet_v2.web_subnet]
 }
 
 resource "openstack_compute_floatingip_associate_v2" "fip_assoc" {
   count = "2"
-  floating_ip = "${element(openstack_compute_floatingip_v2.fip.*.address, count.index)}"
-  instance_id = "${element(openstack_compute_instance_v2.web_cluster.*.id, count.index)}"
+  floating_ip = element(openstack_compute_floatingip_v2.fip.*.address, count.index)
+  instance_id = element(openstack_compute_instance_v2.web_cluster.*.id, count.index)
 }
 
 output "web-instances" {
@@ -168,7 +169,7 @@ resource "openstack_networking_network_v2" "db_net" {
 
 resource "openstack_networking_subnet_v2" "db_subnet" {
   name = "demo-db-subnet"
-  network_id = "${openstack_networking_network_v2.db_net.id}"
+  network_id = openstack_networking_network_v2.db_net.id
   cidr = "10.0.1.0/24"
   ip_version = 4
   enable_dhcp = "true"
@@ -176,8 +177,8 @@ resource "openstack_networking_subnet_v2" "db_subnet" {
 }
 
 resource "openstack_networking_router_interface_v2" "db-ext-interface" {
-  router_id = "${openstack_networking_router_v2.router.id}"
-  subnet_id = "${openstack_networking_subnet_v2.db_subnet.id}"
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.db_subnet.id
 }
 
 ### [DB instances] ###
@@ -190,18 +191,19 @@ resource "openstack_compute_servergroup_v2" "db_srvgrp" {
 resource "openstack_compute_instance_v2" "db_cluster" {
   name = "demo-db-${count.index+1}"
   count = "2"
-  availability_zone = "${element(var.az_list, count.index)}"
+  availability_zone = element(var.az_list, count.index)
   image_name = "ubuntu-16.04-server-latest"
   flavor_name = "v1-mini-1"
-  network = { 
-    uuid = "${openstack_networking_network_v2.db_net.id}"
+  network  { 
+    uuid = openstack_networking_network_v2.db_net.id
   }
-  key_pair = "${openstack_compute_keypair_v2.demo_keypair.name}"
+  key_pair = openstack_compute_keypair_v2.demo_keypair.name
   scheduler_hints {
-    group = "${openstack_compute_servergroup_v2.db_srvgrp.id}"
+    group = openstack_compute_servergroup_v2.db_srvgrp.id
   }
   security_groups = ["${openstack_compute_secgroup_v2.ssh_sg.name}","${openstack_compute_secgroup_v2.db_sg.name}"]
-  user_data = "${var.cloudconfig_db}"
+  user_data = var.cloudconfig_db
+  depends_on = [openstack_networking_subnet_v2.db_subnet]
 }
 
 output "db-instances" {
