ab87f1b24e27af22b8f66682a02e48b658212793
Joel Lundin <joel.lundin@elastx.se>
2021-10-04 17:15:12 +0200

    updated to support Terraform v1.0.8 and OpenStack provider v1.44

--- terraform.tf
+++ terraform.tf
@@ -34,6 +34,18 @@ packages:
 EOF
 }
 
+### Defining required providers ###
+
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.44.0"
+    }
+  }
+}
+
 
 ### [Elastx Openstack] ###
 
